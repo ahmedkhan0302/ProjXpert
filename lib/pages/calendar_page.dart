808ef63abeadf6748c42795f1454488229480de9
AkaashSamson <akaashsam22@gmail.com>
2024-10-22 21:47:06 +0530

    task and calendar

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 import 'package:projxpert/services/firestore.dart';
 
 class CalendarPage extends StatefulWidget {
@@ -15,137 +16,288 @@ class _CalendarPageState extends State<CalendarPage> {
   String? projectID;
   String? projectName;
   List<Map<String, dynamic>> phases = [];
-  bool isLoading = true; // Add a loading state
+  bool isLoading = true;
+  Map<DateTime, List<String>> phaseDates = {}; // Store events for calendar
+  int currentPhaseIndex = 0;
+  DateTime? _selectedDay;
+  final DateTime _focusedDay = DateTime.now();
+
+  // Define a map to store the colors for each phase
+  Map<String, Color> phaseColors = {
+    'Phase 1': Colors.red,
+    'Phase 2': Colors.blue,
+    'Phase 3': Colors.green,
+    // Add more phases and their corresponding colors here
+  };
 
   @override
   void initState() {
     super.initState();
-    checkUserTeamStatus();
-    checkProjectStatus();
+    checkProjectStatus(); // Fetch project and phases data
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          // title: const Text('Calendar Page'),
-          ),
-      body: Center(
-        child: isLoading
-            ? const CircularProgressIndicator() // Show loader if loading
-            : Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'Project Name: $projectName',
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  const SizedBox(height: 20),
-                  const SizedBox(height: 20),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: phases.length,
-                      itemBuilder: (context, index) {
-                        final phase = phases[index];
-                        return ListTile(
-                          title: Text(phase['phaseName']),
-                          subtitle: Text(
-                              'Start: ${phase['startDate']} \nEnd: ${phase['endDate']}'),
-                        );
+        title: Center(child: Text(projectName ?? 'No project found')),
+      ),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    _buildPhaseSummaryCard(), // Phase Summary Card
+                    const SizedBox(height: 20),
+                    TableCalendar(
+                      firstDay: DateTime.utc(2023, 1, 1),
+                      lastDay: DateTime.utc(2025, 12, 31),
+                      focusedDay: _focusedDay,
+                      calendarFormat: CalendarFormat.month,
+                      eventLoader: _getEventsForDay, // Load events for each day
+                      calendarStyle: const CalendarStyle(
+                        todayDecoration: BoxDecoration(
+                          color: Colors.deepPurpleAccent,
+                          shape: BoxShape.circle,
+                        ),
+                        markerDecoration: BoxDecoration(
+                          color: Colors.green,
+                          shape: BoxShape.circle,
+                        ),
+                        selectedDecoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          shape: BoxShape.circle,
+                        ),
+                        // Customize the appearance of days with events
+                        outsideDaysVisible: false,
+                        defaultDecoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                        ),
+                        weekendDecoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                        ),
+                        holidayDecoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      calendarBuilders: CalendarBuilders(
+                        defaultBuilder: (context, day, focusedDay) {
+                          if (phaseDates.containsKey(day)) {
+                            String phaseName = phaseDates[day]!.first;
+                            Color? phaseColor = phaseColors[phaseName];
+                            if (phaseColor != null) {
+                              return Container(
+                                decoration: BoxDecoration(
+                                  color: phaseColor,
+                                  shape: BoxShape.rectangle,
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    '${day.day}',
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              );
+                            }
+                          }
+                          return null;
+                        },
+                      ),
+                      selectedDayPredicate: (day) {
+                        return isSameDay(_selectedDay, day);
                       },
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
+            ),
+    );
+  }
+
+  /// Builds the phase summary card to display current, previous, and upcoming phases
+  Widget _buildPhaseSummaryCard() {
+    String currentPhaseName =
+        phases.isNotEmpty ? phases[currentPhaseIndex]['phaseName'] : 'N/A';
+    String currentPhaseDeadline = phases.isNotEmpty
+        ? phases[currentPhaseIndex]['endDate'].toString().split(' ')[0]
+        : 'N/A';
+
+    String upcomingPhaseName = (currentPhaseIndex + 1 < phases.length)
+        ? phases[currentPhaseIndex + 1]['phaseName']
+        : 'None';
+    String upcomingPhaseDeadline = (currentPhaseIndex + 1 < phases.length)
+        ? phases[currentPhaseIndex + 1]['endDate'].toString().split(' ')[0]
+        : 'N/A';
+
+    String previousPhaseName = (currentPhaseIndex > 0)
+        ? phases[currentPhaseIndex - 1]['phaseName']
+        : 'None';
+    String previousPhaseDeadline = (currentPhaseIndex > 0)
+        ? phases[currentPhaseIndex - 1]['endDate'].toString().split(' ')[0]
+        : 'N/A';
+
+    return Card(
+      color: Colors.purple.shade100,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildPhaseRow('Current Phase:', currentPhaseName, Colors.red,
+                currentPhaseDeadline),
+            const SizedBox(height: 10),
+            _buildPhaseRow('Upcoming Phase:', upcomingPhaseName, Colors.blue,
+                upcomingPhaseDeadline),
+            const SizedBox(height: 10),
+            _buildPhaseRow('Previous Phase:', previousPhaseName, Colors.yellow,
+                previousPhaseDeadline),
+          ],
+        ),
       ),
     );
   }
 
+  /// Builds a row for each phase detail
+  Widget _buildPhaseRow(
+      String label, String phaseName, Color color, String deadline) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        Expanded(
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: Column(
+              crossAxisAlignment:
+                  CrossAxisAlignment.end, // Align text to the right side
+              children: [
+                Text(
+                  phaseName,
+                  style: TextStyle(
+                    color: color,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  'Deadline: $deadline',
+                  style: const TextStyle(fontSize: 14),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<void> checkProjectStatus() async {
+    setState(() => isLoading = true);
+    await checkUserTeamStatus();
+
+    String userId = FirebaseAuth.instance.currentUser!.uid;
+    bool projectFound = false;
+
+    if (teamID != null) {
+      projectFound = await fetchProjectFromTeam();
+    }
+
+    if (!projectFound) {
+      projectFound = await fetchUserOwnedProject(userId);
+    }
+
+    if (projectFound) {
+      DocumentSnapshot projSnapshot = await FirebaseFirestore.instance
+          .collection('projects')
+          .doc(projectID)
+          .get();
+
+      if (projSnapshot.exists) {
+        setState(() {
+          projectName = projSnapshot['projectName'];
+        });
+      }
+    }
+
+    setState(() => isLoading = false);
+  }
+
   Future<void> checkUserTeamStatus() async {
     String userId = FirebaseAuth.instance.currentUser!.uid;
-
-    // Check if the user is in the user_team collection
     QuerySnapshot userTeamSnapshot = await FirebaseFirestore.instance
         .collection('user_teams')
         .where('userId', isEqualTo: userId)
         .get();
 
     if (userTeamSnapshot.docs.isNotEmpty) {
-      String teamId = userTeamSnapshot.docs.first['teamId'];
       setState(() {
-        teamID = teamId;
+        teamID = userTeamSnapshot.docs.first['teamId'];
       });
     }
   }
 
-  Future<void> checkProjectStatus() async {
-    await checkUserTeamStatus();
-    String userId = FirebaseAuth.instance.currentUser!.uid;
-    if (teamID != null) {
-      QuerySnapshot projectTeamSnapshot = await FirebaseFirestore.instance
-          .collection('teams_projects')
-          .where('teamID', isEqualTo: teamID)
-          .get();
+  Future<bool> fetchProjectFromTeam() async {
+    QuerySnapshot projectTeamSnapshot = await FirebaseFirestore.instance
+        .collection('teams_projects')
+        .where('teamID', isEqualTo: teamID)
+        .get();
 
-      if (projectTeamSnapshot.docs.isNotEmpty) {
-        Firestoreservice firestoreService = Firestoreservice();
-        for (var doc in projectTeamSnapshot.docs) {
-          String projectId = doc['projectID'];
-          bool isCompleted =
-              await firestoreService.isProjectCompleted(projectId);
-          if (!isCompleted) {
-            DocumentSnapshot projSnapshot = await FirebaseFirestore.instance
-                .collection('projects')
-                .doc(projectId)
-                .get();
-            if (projSnapshot.exists) {
-              setState(() {
-                projectName = projSnapshot['projectName'];
-                projectID = projectId;
-              });
-              await getBuildPhaseList(); // Fetch phases after setting projectID
-            }
-            setState(() {
-              isLoading = false; // Stop loading
-            });
-            return;
-          }
-        }
+    for (var doc in projectTeamSnapshot.docs) {
+      String projectId = doc['projectID'];
+      bool isCompleted = await Firestoreservice().isProjectCompleted(projectId);
+
+      if (!isCompleted) {
+        await loadProjectDetails(projectId);
+        return true;
       }
     }
+    return false;
+  }
 
-    // Check if the user is a creator of a team
+  Future<bool> fetchUserOwnedProject(String userId) async {
     QuerySnapshot projSnapshot = await FirebaseFirestore.instance
         .collection('projects')
         .where('ownerId', isEqualTo: userId)
         .get();
 
-    if (projSnapshot.docs.isNotEmpty) {
-      Firestoreservice firestoreService = Firestoreservice();
-      for (var doc in projSnapshot.docs) {
-        String projectId = doc.id;
-        bool isCompleted = await firestoreService.isProjectCompleted(projectId);
-        if (!isCompleted) {
-          setState(() {
-            projectName = doc['projectName'];
-            projectID = projectId;
-          });
-          await getBuildPhaseList(); // Fetch phases after setting projectID
-          break; // Exit the loop once an incomplete project is found
-        }
+    for (var doc in projSnapshot.docs) {
+      String projectId = doc.id;
+      bool isCompleted = await Firestoreservice().isProjectCompleted(projectId);
+
+      if (!isCompleted) {
+        await loadProjectDetails(projectId);
+        return true;
       }
     }
+    return false;
+  }
 
-    setState(() {
-      isLoading = false; // Stop loading
-    });
+  Future<void> loadProjectDetails(String projectId) async {
+    DocumentSnapshot projSnapshot = await FirebaseFirestore.instance
+        .collection('projects')
+        .doc(projectId)
+        .get();
+
+    if (projSnapshot.exists) {
+      setState(() {
+        projectName = projSnapshot['projectName'];
+        projectID = projectId;
+      });
+      await getBuildPhaseList();
+    }
   }
 
   Future<void> getBuildPhaseList() async {
     if (projectID != null) {
-      Firestoreservice firestoreService = Firestoreservice();
       QuerySnapshot scheduleSnapshot =
-          await firestoreService.getScheduleStream(projectID!).first;
+          await Firestoreservice().getScheduleStream(projectID!).first;
 
       List<Map<String, dynamic>> fetchedPhases =
           scheduleSnapshot.docs.map((doc) {
@@ -159,11 +311,30 @@ class _CalendarPageState extends State<CalendarPage> {
       if (mounted) {
         setState(() {
           phases = fetchedPhases;
+          phaseDates = _generatePhaseDates(fetchedPhases);
         });
       }
     }
-    setState(() {
-      isLoading = true; // Start loading
-    });
+  }
+
+  Map<DateTime, List<String>> _generatePhaseDates(
+      List<Map<String, dynamic>> phases) {
+    Map<DateTime, List<String>> dates = {};
+
+    for (var phase in phases) {
+      DateTime startDate = phase['startDate'];
+      DateTime endDate = phase['endDate'];
+
+      for (var day = startDate;
+          day.isBefore(endDate) || day.isAtSameMomentAs(endDate);
+          day = day.add(const Duration(days: 1))) {
+        dates.putIfAbsent(day, () => []).add(phase['phaseName']);
+      }
+    }
+    return dates;
+  }
+
+  List<String> _getEventsForDay(DateTime day) {
+    return phaseDates[day] ?? [];
   }
 }
--- lib/pages/tasks_page.dart
+++ lib/pages/tasks_page.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:projxpert/services/firestore.dart';
 
 class TasksPage extends StatefulWidget {
@@ -14,67 +14,38 @@ class _TasksPageState extends State<TasksPage> {
   final TextEditingController textController = TextEditingController();
   final Firestoreservice firestoreservice = Firestoreservice();
   final user = FirebaseAuth.instance.currentUser;
-  final userid = FirebaseAuth.instance.currentUser!.uid;
+  final userid = FirebaseAuth.instance.currentUser?.uid;
   List<Task> incompleteTasks = [];
   List<Task> completeTasks = [];
 
-  @override
-  void initState() {
-    super.initState();
-    buildTaskLists();
-  }
-
-  Future<void> buildTaskLists() async {
-    QuerySnapshot taskSnapshot =
-        await firestoreservice.getTaskStream(userid).first;
-    List<Task> fetchedIncompleteTasks = [];
-    List<Task> fetchedCompleteTasks = [];
-
-    for (var doc in taskSnapshot.docs) {
-      Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-      String taskId = doc.id;
-      String taskName = data['task'];
-      bool isCompleted = data['done'];
-
-      Task task = Task(id: taskId, name: taskName, isCompleted: isCompleted);
-
-      if (isCompleted) {
-        fetchedCompleteTasks.add(task);
-      } else {
-        fetchedIncompleteTasks.add(task);
-      }
-    }
-
-    if (mounted) {
-      setState(() {
-        incompleteTasks = fetchedIncompleteTasks;
-        completeTasks = fetchedCompleteTasks;
-      });
-    }
-  }
-
   void openTaskDialog({String? docID}) {
     showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-              content: TextField(
-                controller: textController,
-              ),
-              actions: [
-                //button to save
-                ElevatedButton(
-                    onPressed: () {
-                      if (docID == null) {
-                        firestoreservice.addTask(
-                            textController.text, user!.uid);
-                      }
-                      textController.clear();
-                      Navigator.pop(context);
-                      buildTaskLists(); // Refresh the task lists
-                    },
-                    child: const Text("Add"))
-              ],
-            ));
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+          decoration: const InputDecoration(hintText: "Enter Task"),
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              if (textController.text.isNotEmpty) {
+                if (docID == null && user != null) {
+                  firestoreservice.addTask(textController.text, user!.uid);
+                }
+                textController.clear();
+                Navigator.pop(context);
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Task cannot be empty')),
+                );
+              }
+            },
+            child: const Text("Add"),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -88,36 +59,97 @@ class _TasksPageState extends State<TasksPage> {
         backgroundColor: Colors.deepPurple[300],
         child: const Icon(Icons.add),
       ),
-      body: ListView.builder(
-        itemCount: incompleteTasks.length + completeTasks.length,
-        itemBuilder: (context, index) {
-          if (index < incompleteTasks.length) {
-            Task task = incompleteTasks[index];
-            return TaskTile(
-              task: task,
-              onChanged: (bool? value) {
-                setState(() {
-                  task.isCompleted = value ?? false;
-                });
-                firestoreservice.updateTaskStatus(
-                    userid, task.id, task.isCompleted);
-                buildTaskLists(); // Refresh the task lists
-              },
-            );
-          } else {
-            Task task = completeTasks[index - incompleteTasks.length];
-            return TaskTile(
-              task: task,
-              onChanged: (bool? value) {
-                setState(() {
-                  task.isCompleted = value ?? false;
-                });
-                firestoreservice.updateTaskStatus(
-                    userid, task.id, task.isCompleted);
-                buildTaskLists(); // Refresh the task lists
-              },
-            );
+      body: StreamBuilder<QuerySnapshot>(
+        stream: userid != null ? firestoreservice.getTaskStream(userid!) : null,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
           }
+          var tasks = snapshot.data!.docs.map((doc) {
+            Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+            return Task(
+              id: doc.id,
+              name: data['task'],
+              isCompleted: data['done'],
+            );
+          }).toList();
+
+          incompleteTasks = tasks.where((task) => !task.isCompleted).toList();
+          completeTasks = tasks.where((task) => task.isCompleted).toList();
+
+          return ReorderableListView(
+            children: [
+              ...incompleteTasks.map((task) => Dismissible(
+                    key: ValueKey(task.id),
+                    direction: DismissDirection.endToStart,
+                    onDismissed: (direction) {
+                      if (userid != null) {
+                        firestoreservice.deleteTask(userid!, task.id);
+                        setState(() {
+                          incompleteTasks.remove(task);
+                        });
+                      }
+                    },
+                    background: Container(
+                      color: Colors.red,
+                      alignment: Alignment.centerRight,
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: const Icon(Icons.delete, color: Colors.white),
+                    ),
+                    child: TaskTile(
+                      task: task,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          task.isCompleted = value ?? false;
+                        });
+                        if (userid != null) {
+                          firestoreservice.updateTaskStatus(
+                              userid!, task.id, task.isCompleted);
+                        }
+                      },
+                    ),
+                  )),
+              ...completeTasks.map((task) => Dismissible(
+                    key: ValueKey(task.id),
+                    direction: DismissDirection.endToStart,
+                    onDismissed: (direction) {
+                      if (userid != null) {
+                        firestoreservice.deleteTask(userid!, task.id);
+                        setState(() {
+                          completeTasks.remove(task);
+                        });
+                      }
+                    },
+                    background: Container(
+                      color: Colors.red,
+                      alignment: Alignment.centerRight,
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: const Icon(Icons.delete, color: Colors.white),
+                    ),
+                    child: TaskTile(
+                      task: task,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          task.isCompleted = value ?? false;
+                        });
+                        if (userid != null) {
+                          firestoreservice.updateTaskStatus(
+                              userid!, task.id, task.isCompleted);
+                        }
+                      },
+                    ),
+                  )),
+            ],
+            onReorder: (oldIndex, newIndex) {
+              setState(() {
+                if (newIndex > oldIndex) newIndex--;
+                final Task movedTask = incompleteTasks.removeAt(oldIndex);
+                incompleteTasks.insert(newIndex, movedTask);
+                // Update Firestore with new order if necessary
+                // firestoreservice.updateTaskOrder(userid, incompleteTasks);
+              });
+            },
+          );
         },
       ),
     );
@@ -144,8 +176,11 @@ class TaskTile extends StatelessWidget {
       margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
       padding: const EdgeInsets.all(8.0),
       decoration: BoxDecoration(
-        border: Border.all(color: Colors.black),
         borderRadius: BorderRadius.circular(8.0),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 5.0)
+        ],
       ),
       child: ListTile(
         title: Text(task.name),
